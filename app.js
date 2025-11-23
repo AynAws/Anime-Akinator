beda873d1bcc29c0df42d78c09f39b331a487ce8
Ayaan Awais <grimcreeperyt@gmail.com>
2025-11-23 01:13:47 -0500

    Saving before mkaing a big change. Adding array compatability

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ window.app = Vue.createApp({
         return { // app-scope variables
             characters: [],
             originalCharacters: [],
-            current: [],
-            questions: {
-                skin_color: "What is this character's skin color?",
-                hair_color: "What is this character's hair color?",
-                show: "What show is this character from?",
-                initial_age: "How old is this character at the beginning?",
-                final_age: "How old is this character at the end?",
-                sex: "What is this character's sex?"
-            }
+            current: []
         }
     },
     mounted() { // on load, run...
@@ -26,59 +18,82 @@ window.app = Vue.createApp({
     methods: { // functions
         getVaryingProperties(items) { // gets keys from character properties
             const prop = Object.keys(items[0]) // prop is a list of all the remaining keys
+
             return prop.filter(prop => {
-                const values = new Set(items.map(i => i[prop])) // values is a list of all remaining keys without duplicated
+                if (prop === "name") return false
+                const values = new Set(items.map(i => i[prop])) // values is a list of all remaining keys without duplicates
                 return values.size > 1 // returns whether the key is useful or not
             })
         },
         chooseBestProperty(items) { // chooses the best property for the next question
-            const props = this.getVaryingProperties(items) // gets a useful key
-            if (props.length === 0) return null // cuts the function if there are no more useful questions left
-            let best = props[0]
-            let bestCount = new Set(items.map(i => i[best])).size // counts the amount of values in best
-            for (const p of props) {
-                const count = new Set(items.map(i => i[p])).size // counts the amount of values in count
-                if (count > bestCount) { // swaps best for the more effective property
-                    best = p
-                    bestCount = count
-                }
-            }
-            return best
-        },
-        getOptionsForProperty(items, prop) {
-            return [...new Set(items.map(i => i[prop]))] // returns all options for the prop without duplicates
-        },
-        applyAnswer(prop, answer) {
-            this.characters = this.characters.filter(item => item[prop] === answer) // narrows down the list based off of the answer
-        },
-        askNextQuestion() { // returns the next question and the associated options or returns the guessed character
-            const prop = this.chooseBestProperty(this.characters)
-            if (prop) { // asks next quesion
-                const question = this.questions[prop]
-                const options = this.getOptionsForProperty(this.characters, prop)
-                return [question, options]
-            } else {
-                if (this.characters.length === 1) { // returns single character
-                    return ["Your character is " + this.characters[0].name + "."]
-                } else if (this.characters.length === 0) { // returns no characters
-                    return ["No such characters found."]
-                } else { // returns multiple characters
-                    let str = []
-                    for (let i = 0; i < this.characters.length; i++) {
-                        if (i + 1 === this.characters.length) {
-                            str += " and "
-                        } else {
-                            str += ", "
-                        }
-                        str += this.characters[i].name
-                    }
-                    return ["Possible characters are: " + str + "."]
-                }
-            }
+            const prop = this.getVaryingProperties(items) // gets a useful key
+            if (prop.length === 0) return null // cuts the function if there are no more useful questions left
 
+            const total = items.length
+            let bestProp = prop[0]
+            let bestDiff = total
+
+            for (const p of prop) {
+                const values = [...new Set(items.map(i => i[p]))]
+                let bestPropDiff = total
+
+                for (const value of values) {
+                    const count = items.filter(item => item[p] === value).length
+                    const diff = Math.abs(total / 2 - count)
+                    if (diff < bestPropDiff) bestPropDiff = diff
+                }
+
+                if (bestPropDiff < bestDiff) {
+                    bestDiff = bestPropDiff
+                    bestProp = p
+                }
+            }
+            return bestProp
+        },
+        getCandidateValue(items, prop) { // picks most effective value to ask about
+            const values = [...new Set(items.map(i => i[prop]))]
+            if (values.length === 0) return null
+            const total = items.length
+            let bestValue = values[0] // bestValue equals names
+            let bestDiff = total // bestDiff is high so that names are always swapped out unless there aren't other options
+            for (const value of values) {
+                const count = items.filter(item => item[prop] === value).length
+                const diff = Math.abs(total / 2 - count)
+                if (diff < bestDiff) {
+                    bestDiff = diff
+                    bestValue = value
+                }
+            }
+            return bestValue
+        },
+        applyAnswer(prop, value, answer) {
+            if (answer) {
+                this.characters = this.characters.filter(item => item[prop] === value)
+            } else {
+                this.characters = this.characters.filter(item => item[prop] !== value)
+            }
+        },
+        askNextQuestion() {
+            const prop = this.chooseBestProperty(this.characters)
+            if (!prop) {
+                if (this.characters.length === 1) return ["Your character is " + this.characters[0].name + "."]
+                else if (this.characters.length === 0) return ["No such character found."]
+                else {
+                    const names = this.characters.map(c => c.name).join(", ")
+                    return ["Possible characters are " + names + "."]
+                }
+            }
+            const value = this.getCandidateValue(this.characters, prop)
+            if (typeof value === "boolean") {
+                return [`Does this character ${prop.replace("_", " ")}?`, prop, value]
+            } else {
+                return [`Is this character's ${prop.replace("_", " ")} ${value || 'unknown or N/A'}?`, prop, value]
+            }
         },
         choose(answer) {
-            this.applyAnswer(this.chooseBestProperty(this.characters), answer)
+            const prop = this.current[1]
+            const value = this.current[2]
+            this.applyAnswer(prop, value, answer)
             this.current = this.askNextQuestion()
         }
     }
